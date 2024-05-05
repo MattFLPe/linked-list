62f18ad17a235bfc61e9ca450cec92341ada33f0
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-04 22:04:16 -0300

    Implement append function: Create a new node with the provided value and adds it to the end of the list

--- linkedlist.js
+++ linkedlist.js
@@ -1,7 +1,23 @@
 class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    };
 
-    function append(value) {
+    append(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
 
+    prepend(value) {
+        const newNode = new Node(value);
+        
     }
 }
 
@@ -11,3 +27,9 @@ class Node {
         this.nextNode = nextNode;
     }
 }
+
+const myList = new LinkedList();
+myList.append(1);
+myList.append(2);
+myList.append(3);
+console.log(myList);
